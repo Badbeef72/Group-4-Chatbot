9bd68c157f08e12fef3d3173e9df58373ce900f1
Badbeef72 <danps311@hotmail.co.uk>
2019-10-19 02:04:20 +0100

    added bmi calculator
    half done, bot isn't asking for weight

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# Main file for the bot.
 '''
     Tutorial from:
     https://www.devdungeon.com/content/make-discord-bot-python
@@ -5,37 +6,55 @@
 import discord # Imports the Discord API.
 import random # Imports Python's "random" library.
 import store # Imports the store.py file for lists of responses.
-import requests # Imports thenrequests api, allowing us to code the bot to recieve and respond to requests.
+import bmi #Imports the bmi.py file to calculate a person's BMI.
+#import requests # Imports the requests api, allowing us to code the bot to recieve and respond to requests.
 
-TOKEN = 'NjMyMzE4MzgwMTI4NjAwMDg0.XamuuA.KUfrcz_EhTFocY8C1eS7PSeOZu4' # Bot's unique ID.
+TOKEN = 'NjMyMzE4MzgwMTI4NjAwMDg0.XapN2Q.k30CS29SNQ_56D8w7yuiNbZzFmo' # Bot's unique ID.
 
 client = discord.Client()
 
 @client.event
-async def on_message(message): # If the user sends a message, do something.
-    message_list = message.content.lower() # Converts all of the message to lower-case, so that if capitals are used, the bot can detect the word anyway.
+# If the user sends a message, do something.
+async def on_message(message):
+    # Converts all of the message to lower-case, so that if capitals are used, the bot can detect the word anyway.
+    message_list = message.content.lower()
     message_list = message_list.split()
-    print(message_list) # Prints the message to the command line for error handling.
+    # Prints the message to the command line for error handling.
+    print(message_list)
     if message.author == client.user:
         return
+    elif message.content.lower() == "hello there":
+        await message.channel.send('General Kenobi!')
 
-    if any(item in store.detectable_greetings for item in message_list): # If user sends "hello", the bot will reply with "Hello (name of user)!" + A random greeting.
+    # If user sends a greeting, the bot will reply with "Hello (name of user)!" + A random greeting.
+    elif any(item in store.detectable_greetings for item in message_list): 
         await message.channel.send('Hello {0.author.mention}! '.format(message) + random.choice(store.list_of_greetings))
+        msg1 = await client.wait_for('message')
+        msg1_list = msg1.content.lower()
+        msg1_list = msg1_list.split()
 
-    elif "good" in message_list:
-        await message.channel.send(random.choice(store.list_of_goods) + " Do you require my assistance?")
-        assistance_question = 1
+        # If user sends a confirming word, i.e: "good", the bot will reply with a random confirmation statement + "Do you require my assistance?".
+        if any(item in store.detectable_positives for item in msg1_list):
+            await message.channel.send(random.choice(store.list_of_goods) + ' Do you require my assistance?'.format(msg1))
+            msg2 = await client.wait_for('message')
+            msg2_list = msg2.content.lower()
+            msg2_list = msg2_list.split()
 
-    elif assistance_question == 1:
-        if "yes" in message_list:
-            await message.channel.send('What do you need help with?')
-            assistance_question = 0
-
-    else: # If anything else is said by the user, the bot outputs a random line describing "I don't know what you mean.".
-        await message.channel.send(random.choice(store.list_of_sorrys))
+            # If the user says yes, the bot will reply with "What with?".
+            if any(item in store.detectable_yes for item in msg2_list):
+                await message.channel.send(random.choice(store.list_of_yes).format(msg2))
+                msg3 = await client.wait_for('message')
+                msg3_list = msg3.content.lower()
+                msg3_list = msg3_list.split()
+                if "bmi" in msg3_list:
+                    await bmi.bmi_calculator(message)
+    # If anything else is said by the user, the bot outputs a random line describing "I don't know what you mean.".
+    #else: 
+        #await message.channel.send(random.choice(store.list_of_sorrys))
 
 @client.event
-async def on_ready(): # Prints successful login.
+# Prints successful login.
+async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
--- store.py
+++ store.py
@@ -1,6 +1,10 @@
+# File for storing lists of potential input/output strings.
 detectable_greetings = ["hello", "hi", "yo", "sup"]
+detectable_positives = ["good", "fine", "great", "alright"]
+detectable_yes = ["yes", "yup", "yeah"]
+
 list_of_greetings = ["I hope you have been well?", "How are you?", "Everything going well?", "What's up?", "How's it going?"]
 list_of_sorrys = ["Sorry, I don't understand what you mean.", "I'm not sure I quite understood that.", "I don't know what you mean, sorry."]
 list_of_goods = ["That's good to hear!", "Nice to hear!", "That's good!"]
-list_of_yes = ["What would you like help with?", "Great! Where shall we begin?", "I will do my best to help with whatever I can."]
+list_of_yes = ["What would you like help with?", "Great! Where shall we begin?"]
 list_of_no = ["OK, enjoy your day.", "I will be here if you change your mind.", "No problem, enjoy the rest of the day."]
