840dcaa850fafafd9242a3b5e14544c8cfdaed61
TheiosNikolakis <49765714+TheiosNikolakis@users.noreply.github.com>
2019-10-31 18:42:17 +0000

    updated mealprep

--- mealPrep(Niko's messy code - under construction).py
+++ mealPrep(Niko's messy code - under construction).py
@@ -4,9 +4,10 @@ import requests
 import store
 import nltk
 from nltk import *
+nltk.download('punkt')
 
 
-token = 'NjMyMzE4MzgwMTI4NjAwMDg0.Xbg_vw.fC5ZxaDY2T7DJhinEOULLnSoq00'
+token = 'NjMyMzE4MzgwMTI4NjAwMDg0.XbsNhQ.3QSZjHfHY_170vijeFhzBmDZMZg'
 client = discord.Client()
 
 @client.event
@@ -16,18 +17,18 @@ async def on_message(message):
     print(tokenized_word)
     if message.author == client.user:
         return
-    elif any(item in store.detectable_greetings for item in tokenized_word):
+    if any(item in store.detectable_greetings for item in tokenized_word):
         await message.channel.send('Hello {0.author.mention}! '.format(message) + random.choice(store.list_of_greetings))
-        msg = await client.wait_for('message'.lower())
-        tokenized_word = word_tokenize(msg)
-        print(tokenized_word)
-        if any(item in store.meal_prep_keywords for item in tokenized_word):
-            await message.channel.send('I can help you to prepare a meal')
-            msg = await client.wait_for('message'.lower())
-            tokenized_word = word_tokenize(msg)
-            print(tokenized_word)
-            if "lunch" in tokenized_word:
-                await message.channel.send("I can give you a meal for lunch")
+        msg = str(await client.wait_for('message'.lower()))
+        tokenized_word1 = word_tokenize(msg)
+        print(tokenized_word1)
+    if any(item in store.meal_prep_keywords for item in tokenized_word or tokenized_word1):
+        await message.channel.send('I can help you to prepare a meal')
+        msg1 = str(await client.wait_for('message'.lower()))
+        tokenized_word2 = word_tokenize(msg1)
+        print(tokenized_word2)
+        if "lunch" in tokenized_word2:
+            await message.channel.send("I can give you a meal for lunch")
 
 
 @client.event
