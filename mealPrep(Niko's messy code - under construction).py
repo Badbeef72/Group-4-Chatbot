8162417521866659f1e5d9082ed03ba84bda7397
TheiosNikolakis <49765714+TheiosNikolakis@users.noreply.github.com>
2019-10-31 16:26:47 +0000

    bug fix

--- mealPrep(Niko's messy code - under construction).py
+++ mealPrep(Niko's messy code - under construction).py
@@ -3,37 +3,31 @@ import random
 import requests
 import store
 import nltk
-
 from nltk import *
 
 
-token = 'NjMyMzE4MzgwMTI4NjAwMDg0.XbGoYA.UmOKeAH3vHymFO4BUgpwaoYwfVQ'
-
+token = 'NjMyMzE4MzgwMTI4NjAwMDg0.Xbg_vw.fC5ZxaDY2T7DJhinEOULLnSoq00'
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    tokenized_word = pos_tag(word_tokenize(message.content.lower()))
+    messageLowered = message.content.lower()
+    tokenized_word = word_tokenize(messageLowered)
     print(tokenized_word)
-    if any(item in store.detectable_greetings for item in tokenized_word):
+    if message.author == client.user:
+        return
+    elif any(item in store.detectable_greetings for item in tokenized_word):
         await message.channel.send('Hello {0.author.mention}! '.format(message) + random.choice(store.list_of_greetings))
         msg = await client.wait_for('message'.lower())
         tokenized_word = word_tokenize(msg)
         print(tokenized_word)
-
-
-@client.event
-async def mealPrep(message):
-    tokenized_word = pos_tag(word_tokenize(message.content.lower()))
-    print(pos_tag(tokenized_word))
-    if any(item in store.meal_prep_keywords for item in tokenized_word):
-        await on_message.channel.send('I can help you to prepare a meal')
-        msg = await client.wait_for('message'.lower())
-        tokenized_word = tokenized_word(msg)
-        Prints(tokenized_word)
-
-
-
+        if any(item in store.meal_prep_keywords for item in tokenized_word):
+            await message.channel.send('I can help you to prepare a meal')
+            msg = await client.wait_for('message'.lower())
+            tokenized_word = word_tokenize(msg)
+            print(tokenized_word)
+            if "lunch" in tokenized_word:
+                await message.channel.send("I can give you a meal for lunch")
 
 
 @client.event
