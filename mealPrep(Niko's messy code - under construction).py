6011f4c5d5d5275c10c36768e6b8dc88e0fa6805
TheiosNikolakis <49765714+TheiosNikolakis@users.noreply.github.com>
2019-11-13 22:16:00 +0000

    Update mealPrep(Niko's messy code - under construction).py

--- mealPrep(Niko's messy code - under construction).py
+++ mealPrep(Niko's messy code - under construction).py
@@ -3,18 +3,12 @@ import random
 import requests
 import store
 import py_edamam
-from py_edamam import PyEdamam #Credit:https://github.com/JarbasAl/py_edamam and https://www.edamam.com/"
+from py_edamam import PyEdamam # Credit:https://github.com/JarbasAl/py_edamam and https://www.edamam.com/"
 import nltk
-import json
 from nltk import *
 nltk.download('punkt')
 
-r = requests.get('https://api.edamam.com/search?app_id=${f66d3fcf}&app_key=${f4f09a804c47eac41b04d4b0bc6acb08')
-print(r.status_code)
-print(r.text)
-print(r.peek)
-
-token = 'NjMyMzE4MzgwMTI4NjAwMDg0.Xb9O7g.oz1dRI8uGJtFkMsDwgBPCdDM0a0'
+token = 'NjMyMzE4MzgwMTI4NjAwMDg0.XcAPGQ.fvUDTEx0nmz4TPILwAxu46rcFNY'
 client = discord.Client()
 e = PyEdamam(
             recipes_appid='f66d3fcf',
@@ -27,38 +21,36 @@ async def on_message(message):
     print(tokenized_word)
     if message.author == client.user:
         return
-    if any(item in store.detectable_greetings for item in tokenized_word):
-        await message.channel.send('Hello {0.author.mention}! '.format(message) + random.choice(store.list_of_greetings))
-        msg = str(await client.wait_for('message'.lower()))
-        tokenized_word = word_tokenize(msg)
-        print(tokenized_word)
-    if any(item in store.meal_prep_keywords for item in tokenized_word):
-        await message.channel.send('I can help you to prepare a meal')
-        msg = str(await client.wait_for('message'.lower()))
-        tokenized_word = word_tokenize(msg)
-        print(tokenized_word)
     if "breakfast" in tokenized_word:
-        await message.channel.send("I will create a breakfast  meal for you")
-        for recipe in e.search_recipe("breakfast") :
-            await message.channel.send("The recipe that you requested: " + str(recipe))
-            await message.channel.send("This recipe has " + str(round(recipe.calories, 2)) + " calories")
-            await message.channel.send(recipe.url)
+        await message.channel.send("What would you like to eat for breakfast?")
+        msg = await client.wait_for('message')
+        new_msg = str(msg.content.lower())
+        # Start of modified code. Original source: https://github.com/JarbasAl/py_edamam/blob/master/example.py #
+        for recipe in e.search_recipe(new_msg): # Users inputs determines the search of the recipe
+            await message.channel.send("The recipe that you requested: " + str(recipe)) # Chatbot shows requested recipe to the user #
+            await message.channel.send("This recipe has " + str(round(recipe.calories, 2)) + " calories") # Chatbot shows calories rounded up to 2 decimal places and displays it to the user#
+            await message.channel.send(recipe.url) # Chatbot shows URL for the recipe #
             break
+        # End of modified code #
     if "lunch" in tokenized_word:
-        await message.channel.send("I will create a lunch meal for you")
-        for recipe in e.search_recipe("lunch"):
+        await message.channel.send("What would you like to eat for lunch?")
+        msg = await client.wait_for('message')
+        new_msg = str(msg.content.lower())
+        for recipe in e.search_recipe(new_msg):
             await message.channel.send("The recipe that you requested: " + str(recipe))
             await message.channel.send("This recipe has " + str(round(recipe.calories, 2)) + " calories")
             await message.channel.send(recipe.url)
             break
     if "dinner" in tokenized_word:
-        await message.channel.send("I will create a dinner meal for you")
-        for recipe in e.search_recipe("dinner"):
+        await message.channel.send("What would you like to eat for dinner?")
+        msg = await client.wait_for('message')
+        new_msg = str(msg.content.lower())
+        for recipe in e.search_recipe(new_msg):
             await message.channel.send("The recipe that you requested: " + str(recipe))
             await message.channel.send("This recipe has " + str(round(recipe.calories, 2)) + " calories")
             await message.channel.send(recipe.url)
             break
-
+    # End of modified code #
 
 @client.event
 async def on_ready(): # Prints successful login.
